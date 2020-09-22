a6252a92631dda276567ad06a5846347bbf26998
narinn-star <skfls2618@naver.com>
2020-09-22 23:48:56 +0900

    20.09.22

--- Review & Study/p114, 10번.c
+++ Review & Study/p114, 10번.c
@@ -9,4 +9,13 @@ int main() {
 	printf("몸무게(kg)를 입력하세요.");
 	scanf_s("%.1lf", &height);
 
+	double feet, pound, standardW, swkg;
+	feet = height * 30.48;
+	pound = weight * 2.20459;
+	standardW = (height - 100) * 0.9;
+	swkg = weight - standardW;
+
+	printf("당신의 키는 %.1lfcm (%.1lffeet)입니다.\n", height, feet);
+	printf("당신의 체중은 %.1lfkg (%.1lflb)입니다.\n", weight, pound);
+	printf("표준 체중 %.1lf과의 차이는 %.1lfkg입니다.", standardW, swkg);
 }

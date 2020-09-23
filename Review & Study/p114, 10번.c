689ff041184ba537f40d66a9c3dc50145bc15c1a
narinn-star <skfls2618@naver.com>
2020-09-23 23:48:27 +0900

    20.09.23

--- Review & Study/p114, 10번.c
+++ Review & Study/p114, 10번.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
 
 int main() {
+
 	double weight, height;
 
 	printf("키(cm)를 입력하세요.");
-	scanf_s("%.1lf", &weight);
+	scanf_s("%lf", &height);
 
 	printf("몸무게(kg)를 입력하세요.");
-	scanf_s("%.1lf", &height);
+	scanf_s("%lf", &weight);
 
-	double feet, pound, standardW, swkg;
-	feet = height * 30.48;
+	double feet, pound;
+	
+	feet = height * 0.032808;
 	pound = weight * 2.20459;
-	standardW = (height - 100) * 0.9;
-	swkg = weight - standardW;
+	
+	printf("당신의 키는 %.1lf cm (%.1lf feet)입니다.\n", height, feet);
+	printf("당신의 체중은 %.1lf kg (%.1lf lb)입니다.\n", weight, pound);
 
-	printf("당신의 키는 %.1lfcm (%.1lffeet)입니다.\n", height, feet);
-	printf("당신의 체중은 %.1lfkg (%.1lflb)입니다.\n", weight, pound);
-	printf("표준 체중 %.1lf과의 차이는 %.1lfkg입니다.", standardW, swkg);
+	double stdweight, weightgap;
+
+	stdweight = (height - 100) * 0.9;
+	weightgap = weight - stdweight;
+
+	printf("표준체중 %.1lf과의 차이는 %+.1lf kg입니다.", stdweight, weightgap);
 }

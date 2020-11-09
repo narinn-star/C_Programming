2290a2ad2411a9f01b2c66ae4a4f309e1ae5bd41
narinn-star <skfls2618@naver.com>
2020-11-09 22:48:17 +0900

    20.11.09

---
+++ BAEKJOON/1차원 배열/BAEKJOON_[No.10818].c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int arr[1000000];
+int main() {
+	int max, min;
+	int N;
+	scanf_s("%d", &N);
+
+	for (int i = 0; i < N; i++) {
+		scanf_s("%d", &arr[i]);
+		if (i == 0) {
+			max = arr[i];
+			min = arr[i];
+		}
+		else {
+			if (max < arr[i])
+				max = arr[i];
+			if (min > arr[i])
+				min = arr[i];
+		}
+	}
+	printf("%d %d", min, max);
+}

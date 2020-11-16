c8410a6c2ddbf17f055bbad57e4c1ffcc807e34b
narinn-star <skfls2618@naver.com>
2020-11-16 23:14:00 +0900

    20.11.16

--- BAEKJOON/1차원 배열/BAEKJOON_[No.2577].c
+++ BAEKJOON/1차원 배열/BAEKJOON_[No.2577].c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 int main() {
-	int A, B, C, sum = 0, result;
+	int A, B, C, sum = 0, result, arr[10] = { 0 };
 	scanf_s("%d %d %d", &A, &B, &C);
 	sum = A * B * C;
 
 	while (sum > 0) {
-		sum % 10 = result;
+		result = sum % 10;
+		arr[result]++;
+		sum /= 10;
+	}
+	
+	for (int i = 0; i < 10; i++) {
+		printf("%d\n", arr[i]);
 	}
 }
